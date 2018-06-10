1d9bd14b00ae78317c864442633ae93f0479a848
Michael Buckley <michaelbuckleyis@gmail.com>
2018-06-09 22:55:24 -0400

    Work on validation and roll functions.

--- src/utils/errorcheck.js
+++ src/utils/errorcheck.js
@@ -1,17 +1,4 @@
-// Atacker starting armies: must be int >= 2
-// Defender starting armies: must be int >=1
-
-// Attacker dice rolled: int 1-3 & number must be < number of starting armies
-// Defender dice rolled: int 1-2 and & number must be <= number of starting armies
-
-// Battle: Higher number wins. If a tie, defender wins. If multiple dice, compare highest with highest.
-
-// Roll options:
-// 1) stop at x number of armies
-// 2) stop when a certain ratio of attacker/defender armies reached
-// 3) continue until end.
-
-// activeRoll probably doesn't belong in Redux state but in function that is doing the rolling.
+// SAMPLE REDUX STATE
 {
   rawRollInfo: {
     "attackArmies": 10,
@@ -22,21 +9,22 @@
     "stopDifferential": ''
   },
   "errorMessages": {
-    // stuff to go here, might be an array instead of object.
-  },
-  "activeRoll": {
-    "currentAttackArmies": 10,
-    "currentDefendArmies": 5,
-    "realAttackNum": 3,
-    "realDefendNum": 2
+    "attackArmies": '',
+    "defendArmies": '',
+    "desiredAttackNum": '',
+    "desiredDefendNum": '',
+    "stopNum": '',
+    "stopDifferential": ''
   },
   "results" {
     "attack": 4,
     "defense": 0,
     "notes": ''
+    // probably need to add a few things
   }
 }
 
+// Maps form/state id to error description for that item
 const getErrorDescripion = {
   "attackArmies": ["attackArmies", "The number of attack armies must be a whole number greater than or equal to 2."],
   "defendArmies": ["defendArmies", "The number of defensive armies must be a whole number greater than or equal to 1."],
@@ -46,32 +34,13 @@ const getErrorDescripion = {
   "stopDifferential": ["stopDifferential", "The stop differential must be an integer, and it must be less than the current differential."]
 };
 
-// I'm not doing an error check for rolls being too great (say 3 rolls but 2 armies) because this will be automatically adjusted in the roll process. However, I should probably add a note saying how the roll number will automatically be adjusted down if needed to follow game rules and to satisfy stop criteria.
-
-// perhaps return errors array no matter what. Then the length of that array can be check by the calling function to decide what to do.
-
-// Also some sort of sanity check on armies to avoid super high numbers?
 
 
 
-// NOTE June 5: parseInt returns integer of NaN. An empty string is NaN (but using Number it is converted to zero.) There are some gottchas for very large and small numbers but it should work for what I need. Also, I could use parseFloat, but might just be simpler to convert to an int then returning an error for a decimal. Also, be sure to specify a radix. Generally assumes 10, but a leading zero will make it assume 8.
+// Function makes sure that all form values are valid and follow game rules. Returns an array. If empty, then there were no errors. If not empty, the array will contain an array for each error found. Each array will contain the name of the form/state value where the error occured and a message about the error. This should be returned to the user.
 validateForm = () => {
 
-  // parsedRollInfo is a bit complex, but what it does is convert the rawRollInfo values in state into a new object that contains the same keys, but the values have either been converted from strings to integers if a valid input given, to NaN if a bogus value was given, or an empty string if belonging to the stopNum or stopDifferential keys (as those are optional fields and '' is valid there).
-  // Example:
-  // {
-  //   "desiredAttackNum": "3",
-  //   "desiredDefendNum": "sdjfsdj",
-  //   "stopNum": ''
-  // }
-  //
-  // becomes =>
-  //
-  // {
-  //   "desiredAttackNum": 3,
-  //   "desiredDefendNum": Nan,
-  //   "stopNum": ''
-  // }
+  // parsedRollInfo is a bit complex, but what it does is convert the rawRollInfo values in state into a new object that contains the same keys, but the values have either been converted from strings to integers if a valid input given, to NaN if a bogus value was given, or an empty string if belonging to the stopNum or stopDifferential keys (as those are optional fields and '' is valid there). Example: {"desiredAttackNum": "3", "desiredDefendNum": "sdjfsdj", "stopNum": ''} becomes => {"desiredAttackNum": 3, "desiredDefendNum": Nan, "stopNum": ''}.
   let parsedRollInfo = Object.assign({}, ...Object.keys(state.rawRollInfo).map(function(key) {
 
     let parsedValue = ((key === "stopNum" || key === "stopDifferential") && state.rawRollInfo[key] === '') ? '' : parseFloat(state.rawRollInfo[key]);
@@ -83,9 +52,10 @@ validateForm = () => {
 
   let errors = [];
 
-  // First round of error checks just makes sure all values are integers. Return if any of these errors are found. It doesn't make sense to do the second round of error checks if something fails here because that will likely return too many errors to be useful.
+  // First round of error checks just makes sure all values are integers or empty strings (where allowed). Return if any of these errors are found. It doesn't make sense to do the second round of error checks if something fails here because that will likely return too many errors to be useful.
   Object.keys(parsedRollInfo).forEach(key) {
-    if (isNaN(parsedRollInfo[key]) || !Number.isInt(parsedRollInfo[key])) {
+    if (parsedRollInfo[key] !== '' &&
+    (isNaN(parsedRollInfo[key]) || !Number.isInteger(parsedRollInfo[key]))) {
       errors.push(getErrorDescripion[key]);
     }
   }
@@ -96,10 +66,7 @@ validateForm = () => {
   }
 
 
-
-
-
-  // Second level error checking. At this point we know all fields that need to be filled in are filled in and contain an integer. Now need to verify that those numbers make sense/follow rules.
+  // Second level error checking. At this point we know all fields that need to be filled in are filled in and contain an integer or empty string (where allowed). Now need to verify that those numbers make sense/follow rules.
   if (parsedRollInfo["attackArmies"] < 2) {
     errors.push(getErrorDescripion["attackArmies"]);
   };
@@ -134,9 +101,6 @@ validateForm = () => {
 
 
 
-// At this point the calling function will get the error array returned to it. If it is empty than all is good and proceed with rolling. If it isn't empty, don't roll and change state to reflect errors so they can be displayed. Each entry in the error array is itself an array with an id and a description. The id can be used to target where to display the error, and the descriptio is the message to display.
-
-
 
 
 functionCalledWhenButtonClicked = () => {
@@ -149,11 +113,23 @@ functionCalledWhenButtonClicked = () => {
   }
 
   // At this point we can proceed with the roll process becuase everything is valid.
-
+  // roll function here
 };
 
 
 
+// I'm thinking a recursive function like below. Would be called with the validated values from state. Each roll round will return an update object in the same form as the state (but not changing state itself). The can continue function takes that object and determines whether roll can continue. If can't continue decide how to communicate that message and display.
+
+
+pseudoRollFunc() {
+  let result = roll();
+  if (canContinue(result)) {
+    return pseudoRollFunc(result);
+  } else {
+    return result;
+  }
+}
+// Perhaps roll() can return an array with something like [true/false, {}, message(optional)];
 
 
 
