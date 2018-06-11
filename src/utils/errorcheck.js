8dfeaeafbbbb2aac0e5e0f01cd5d1e84505a6427
Michael Buckley <michaelbuckleyis@gmail.com>
2018-06-10 22:56:19 -0400

    Additional work on roll function.

--- src/utils/errorcheck.js
+++ src/utils/errorcheck.js
@@ -3,20 +3,21 @@
   rawRollInfo: {
     "attackArmies": 10,
     "defendArmies": 5,
-    "desiredAttackNum": 3,
-    "desiredDefendNum": 2,
+    "attackRollNum": 3,
+    "defendRollNum": 2,
     "stopNum": '',
     "stopDifferential": ''
   },
   "errorMessages": {
     "attackArmies": '',
     "defendArmies": '',
-    "desiredAttackNum": '',
-    "desiredDefendNum": '',
+    "attackRollNum": '',
+    "defendRollNum": '',
     "stopNum": '',
     "stopDifferential": ''
   },
   "results" {
+    "history": [].
     "attack": 4,
     "defense": 0,
     "notes": ''
@@ -28,8 +29,8 @@
 const getErrorDescripion = {
   "attackArmies": ["attackArmies", "The number of attack armies must be a whole number greater than or equal to 2."],
   "defendArmies": ["defendArmies", "The number of defensive armies must be a whole number greater than or equal to 1."],
-  "desiredAttackNum": ["desiredAttackNum", "Attack can only roll 1, 2 or 3 dice, and the number of dice rolled must be less than the number of attack armies."],
-  "desiredDefendNum": ["desiredDefendNum", "Defense can only roll 1 or 2 dice, and the number of dice rolled must not exceed the number of defense armies."],
+  "attackRollNum": ["attackRollNum", "Attack can only roll 1, 2 or 3 dice, and the number of dice rolled must be less than the number of attack armies."],
+  "defendRollNum": ["defendRollNum", "Defense can only roll 1 or 2 dice, and the number of dice rolled must not exceed the number of defense armies."],
   "stopNum": ["stopNum", "Stop number must be a whole number that is less than the current number of attack armies and greater than 0."],
   "stopDifferential": ["stopDifferential", "The stop differential must be an integer, and it must be less than the current differential."]
 };
@@ -40,7 +41,7 @@ const getErrorDescripion = {
 // Function makes sure that all form values are valid and follow game rules. Returns an array. If empty, then there were no errors. If not empty, the array will contain an array for each error found. Each array will contain the name of the form/state value where the error occured and a message about the error. This should be returned to the user.
 validateForm = () => {
 
-  // parsedRollInfo is a bit complex, but what it does is convert the rawRollInfo values in state into a new object that contains the same keys, but the values have either been converted from strings to integers if a valid input given, to NaN if a bogus value was given, or an empty string if belonging to the stopNum or stopDifferential keys (as those are optional fields and '' is valid there). Example: {"desiredAttackNum": "3", "desiredDefendNum": "sdjfsdj", "stopNum": ''} becomes => {"desiredAttackNum": 3, "desiredDefendNum": Nan, "stopNum": ''}.
+  // parsedRollInfo is a bit complex, but what it does is convert the rawRollInfo values in state into a new object that contains the same keys, but the values have either been converted from strings to integers if a valid input given, to NaN if a bogus value was given, or an empty string if belonging to the stopNum or stopDifferential keys (as those are optional fields and '' is valid there). Example: {"attackRollNum": "3", "defendRollNum": "sdjfsdj", "stopNum": ''} becomes => {"attackRollNum": 3, "defendRollNum": Nan, "stopNum": ''}.
   let parsedRollInfo = Object.assign({}, ...Object.keys(state.rawRollInfo).map(function(key) {
 
     let parsedValue = ((key === "stopNum" || key === "stopDifferential") && state.rawRollInfo[key] === '') ? '' : parseFloat(state.rawRollInfo[key]);
@@ -75,16 +76,16 @@ validateForm = () => {
     errors.push(getErrorDescripion["defendArmies"]);
   };
 
-  if (parsedRollInfo["desiredAttackNum"] > parsedRollInfo["attackArmies"] - 1 ||
-      parsedRollInfo["desiredAttackNum"] > 3 ||
-      parsedRollInfo["desiredAttackNum"] < 1) {
-        errors.push(getErrorDescripion["desiredAttackNum"]);
+  if (parsedRollInfo["attackRollNum"] > parsedRollInfo["attackArmies"] - 1 ||
+      parsedRollInfo["attackRollNum"] > 3 ||
+      parsedRollInfo["attackRollNum"] < 1) {
+        errors.push(getErrorDescripion["attackRollNum"]);
   };
 
-  if (parsedRollInfo["desiredDefendNum"] > parsedRollInfo["defendArmies"] ||
-      parsedRollInfo["desiredDefendNum"] < 1 ||
-      parsedRollInfo["desiredDefendNum"] > 2) {
-        errors.push(getErrorDescripion["desiredDefendNum"]);
+  if (parsedRollInfo["defendRollNum"] > parsedRollInfo["defendArmies"] ||
+      parsedRollInfo["defendRollNum"] < 1 ||
+      parsedRollInfo["defendRollNum"] > 2) {
+        errors.push(getErrorDescripion["defendRollNum"]);
   };
 
   if (parsedRollInfo["stopNum"] && (parsedRollInfo["stopNum"] < 1 || parsedRollInfo["stopNum"] >= parsedRollInfo["attackArmies"]) {
@@ -121,40 +122,69 @@ functionCalledWhenButtonClicked = () => {
 // I'm thinking a recursive function like below. Would be called with the validated values from state. Each roll round will return an update object in the same form as the state (but not changing state itself). The can continue function takes that object and determines whether roll can continue. If can't continue decide how to communicate that message and display.
 
 
-pseudoRollFunc() {
+rollTillStop = (obj) => {
   let result = roll();
-  if (canContinue(result)) {
-    return pseudoRollFunc(result);
+
+  // Here I need to take info from roll and 1) adjust obj with new values, 2) determine if stop criteria have been met, 3) determine if roll numbers need to be adjusted down, 4) update history. Then I can continue with the recursive part.
+
+  if (canContinue(newObj)) {
+    return rollTillStop(newObj);
   } else {
-    return result;
+    return newObj;
   }
 }
 // Perhaps roll() can return an array with something like [true/false, {}, message(optional)];
 
 
-
+// Returns an object with the roll numbers and differentials for attack/defend
 roll () => {
-  return Math.floor(Math.random() * 7;
-};
-
-playARound () => {
   let attackRolls = [];
   let defendRolls = [];
+  let attackResult = 0;
+  let defendResult = 0;
 
-  for (let i = 1; i <=realAttackNum; i++) {
-    attackRolls.push[roll()];
+  for (let i = 1; i <=attackNum; i++) {
+    attackRolls.push[Math.floor(Math.random() * 7)];
+  }
+  attackRolls.sort((a, b) => a - b); // function ensures sorts by number and not string value
+
+  for (let i = 1; i <=defendNum; i++) {
+    defendRolls.push[Math.floor(Math.random() * 7)];
+  }
+  defendRolls.sort((a, b) => a - b);
+
+  // AT THIS POINT WE HAVE TWO ARRAYS WITH THEIR ROLLS SORTED IN ASCENDING ORDER
+
+  // Compare highest to highest
+  (defendRolls[defendRolls.length - 1] >= attackRolls[attackRolls.length - 1]) ? attackResult-- : defendResult--;
+
+  // If there is a secondary dice comparison, do it
+  if (attackNum >= 2 and defendNum === 2) {
+    (defendRolls[defendRolls.length - 2] >= attackRolls[attackRolls.length - 2]) ? attackResult-- : defendResult--;
   }
 
-  for (let i = 1; i <=realDefendNum; i++) {
-    defendRolls.push[roll()];
-  }
-
-  if (defendRolls === 1) {
-    return (Math.max(...attackRolls) > defendRolls) ? "attack wins" : "defend wins";
-  } else {
-
-  }
+  return {attackRolls, defendRolls, attackResult, defendResult};
 };
+
+
+// let newAttackValue = obj.attackArmies + attackResult;
+// let newDefendValue = obj.defendArmies + defendResult;
+//
+// // returns object equal to passed in object but with updated attack/defend armies based on roll result.
+// return Object.assign({}, obj, {"attackArmies": newAttackValue, "defendArmies": newDefendValue});
+
+
+
+// Sample object form for rolling info:
+rollResults: {
+  "attackArmies": 10,
+  "defendArmies": 5,
+  "AttackRollNum": 3,
+  "defendRollNum": 2,
+  "stopNum": '',
+  "stopDifferential": ''
+  "history": []
+},
 
 
 // Is game over?
@@ -183,7 +213,7 @@ atStopDifferential () => {
 
 // NOT FINISHED, NOT SURE IF CORRECT
 getAttackNum = () => {
-  let max = (attackArmies > desiredAttackNum) ? desiredAttackNum : attackArmies - 1;
+  let max = (attackArmies > attackRollNum) ? attackRollNum : attackArmies - 1;
 
   if (stopNum && (attackArmies - max < stopNum)) {
 
@@ -191,7 +221,7 @@ getAttackNum = () => {
 }
 
 getDefendNum () => {
-  if (desiredDefendNum === 2 && defendArmies >= 2) {
+  if (defendRollNum === 2 && defendArmies >= 2) {
     return 2;
   }
   return 1;
